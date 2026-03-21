80ce7f53248bdd1a5825fb48ad36e862607ec63a
Thalath Tun <tunthalath@gmail.com>
2026-03-21 14:40:27 +0700

    del form_widget from week1

--- form_widget/android/app/src/main/kotlin/com/example/form_widget/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.form_widget
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- form_widget/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:form_widget/widgets/root_app.dart';
-
-void main()
-{
-  runApp(
-    RootApp()
-  );
-}
