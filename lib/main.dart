5f25a91e3909594f0678b6f51bfb6f5bd072e293
Thalath Tun <tunthalath@gmail.com>
2026-03-28 18:18:29 +0700

    API Fetch: https://fakestoreapi.com/products

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+
+import 'dart:convert';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Product Detail'),
     );
   }
 }
@@ -41,17 +47,27 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: .fromRGBO(100, 50, 2500, 0.5),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        centerTitle: true,
       ),
       body: _buildBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+
+  Future<List<Map<String, dynamic>>?> _readAPIS() async
+  {
+    http.Response response = await http.get(Uri.parse("https://fakestoreapi.com/products"));
+    if (response.statusCode == 200)
+    {
+      List data = jsonDecode(response.body);
+      List<Map<String, dynamic>> items = data.map((x) => x as Map<String, dynamic>).toList();
+      return items;
+    }
+    throw Exception("Failed load API");
+    
   }
 
 
@@ -66,14 +82,14 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _buildBody()
   {
     return Center(
-      child: FutureBuilder<String>(future: _readData(), builder: (content, snapshot){
+      child: FutureBuilder<List<Map<String, dynamic>>?>(future: _readAPIS(), builder: (content, snapshot){
         if (snapshot.hasError){
           return Text("Error ${snapshot.error.toString()}");
         }
       
         if (snapshot.connectionState == ConnectionState.done)
         {
-          return Text(snapshot.data!);
+          return _buildGridView(snapshot.data);
         }
         else
         {
@@ -81,5 +97,35 @@ class _MyHomePageState extends State<MyHomePage> {
         }
       }),
     );
+  }
+
+  Widget _buildGridView(List<Map<String, dynamic>>? items)
+  {
+    if (items == null)
+    {
+      Icon(Icons.hourglass_empty);
+    }
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount:  2,
+        childAspectRatio: 2/3,
+      ), 
+      itemBuilder: (content, index) {
+        Map<String, dynamic> item = items![index];
+
+        return Card(
+          child: Column(
+            children: [
+              Expanded(child: CachedNetworkImage(
+                imageUrl: item['image'],
+                placeholder: (_, _) => Container(color: Colors.grey,),
+                errorWidget: (_,_,_) => Container(color: Colors.grey),
+              )),
+              Text(item['title']),
+            ],
+          ),
+        );
+    });
   }
 }
